af014f2b8722c4f3d6f0e8800b2a93956c031e70
Shidas <ishidas1155@gmail.com>
2021-10-09 17:17:56 +0900

    [AC] abc087 abc087_b

---
+++ atcoder.jp/abc087/abc087_b/Main.py
@@ -0,0 +1,14 @@
+a=int(input().rstrip())
+b=int(input().rstrip())
+c=int(input().rstrip())
+x=int(input().rstrip())
+
+count=0
+
+for i in range(a+1):
+    for j in range(b+1):
+        for k in range(c+1):
+            if (500*i + 100*j + 50*k)==x:
+                count += 1
+
+print(count)
